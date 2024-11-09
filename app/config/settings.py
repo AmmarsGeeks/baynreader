4ae9c85be9ff35bac217e1dc0284aafb3970e839
Ammar Hammouch <codingammarhamdi@gmail.com>
2024-11-09 14:27:22 +0300

    push settings

--- app/config/settings.py
+++ app/config/settings.py
@@ -1,6 +1,9 @@
-import os
-from dotenv import load_dotenv
+import streamlit as st
+import openai
 
-load_dotenv()
-os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = os.getenv("GOOGLE_APPLICATION_CREDENTIALS")
-openai.api_key = os.getenv("OPENAI_API_KEY")
+# Access secrets using st.secrets
+google_credentials_path = st.secrets["GOOGLE_APPLICATION_CREDENTIALS"]
+openai.api_key = st.secrets["OPENAI_API_KEY"]
+
+# Set the environment variable for Google credentials (if required by Google SDK)
+os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = google_credentials_path
